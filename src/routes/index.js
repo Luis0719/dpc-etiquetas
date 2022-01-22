c75103393536ccee1408deaedfc14962f1d59afd
Luis Ayala <luisayala@Luiss-MacBook-Pro.local>
2022-01-22 14:17:08 -0800

    Preview printable pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,29 @@ function setup(app) {
   });
 
   app.post('/preview', async function (req, res) {
+    /*
+      Each page allows up to 5 imgs per row, and 9 rows.
+      45 in total. Split list into pages of 45 elements each.
+    */
     let etiquetas = Object.entries(req.body); // convert from key-value pair, to array of key-values
-    return res.render('preview', { etiquetas });
+    etiquetas = etiquetas.filter(x => x[1] > 0); // Ignore those without a > 0 value
+    etiquetas = etiquetas.reduce((result, x) => { // Expand array. Add n elements of each perfume, where n is defined at x[1]
+      for (let i=0; i<x[1]; i+=1) {
+        result.push(x[0]);
+      }
+
+      return result;
+    }, []);
+
+    // Split into chunks of 45 (items per page)
+    pages = [];
+    for (let i=0; i<etiquetas.length; i+= 45) {
+      pages.push(
+        etiquetas.slice(i, i+45)
+      );
+    }
+
+    return res.render('preview', { pages });
   });
 }
 
