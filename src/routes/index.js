fc777b90ea7e0cbf0fbd5f691993dcbdbe0d0eb0
Luis Ayala <luis.hdza97@gmail.com>
2022-05-27 18:37:34 -0700

    Remove metadata.json file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,26 @@
-const fs = require("fs/promises");
+const { readdir } = require("fs/promises");
 
 function setup(app) {
-  app.get('/', async function (req, res) {
-    const etiquetasRaw = await fs.readFile("src/etiquetas.meta.json");
-    const etiquetas = JSON.parse(etiquetasRaw).sort((a, b) => {
-      return a.name.localeCompare(b.name);
+  app.get("/", async function (req, res) {
+    let etiquetas = await readdir("public/files/etiquetas");
+
+    etiquetas = etiquetas.sort((a, b) => {
+      return a.localeCompare(b);
     });
 
-    return res.render('index', { etiquetas });
+    return res.render("index", { etiquetas });
   });
 
-  app.post('/preview', async function (req, res) {
+  app.post("/preview", async function (req, res) {
     /*
       Each page allows up to 5 imgs per row, and 9 rows.
       45 in total. Split list into pages of 45 elements each.
     */
     let etiquetas = Object.entries(req.body); // convert from key-value pair, to array of key-values
-    etiquetas = etiquetas.filter(x => x[1] > 0); // Ignore those without a > 0 value
-    etiquetas = etiquetas.reduce((result, x) => { // Expand array. Add n elements of each perfume, where n is defined at x[1]
-      for (let i=0; i<x[1]; i+=1) {
+    etiquetas = etiquetas.filter((x) => x[1] > 0); // Ignore those without a > 0 value
+    etiquetas = etiquetas.reduce((result, x) => {
+      // Expand array. Add n elements of each perfume, where n is defined at x[1]
+      for (let i = 0; i < x[1]; i += 1) {
         result.push(x[0]);
       }
 
@@ -27,13 +29,11 @@ function setup(app) {
 
     // Split into chunks of 45 (items per page)
     pages = [];
-    for (let i=0; i<etiquetas.length; i+= 45) {
-      pages.push(
-        etiquetas.slice(i, i+45)
-      );
+    for (let i = 0; i < etiquetas.length; i += 45) {
+      pages.push(etiquetas.slice(i, i + 45));
     }
 
-    return res.render('preview', { pages });
+    return res.render("preview", { pages });
   });
 }
 
